ebcbbe4e6c257306c48e1e1871cb6af1617b5d8e
Jan Tkacik <jan.tkacik@gmail.com>
2015-06-18 13:53:39 +0200

    Color convert

--- nao-module.py
+++ nao-module.py
@@ -1,6 +1,4 @@
 import sys
-
-from PyQt4.QtGui import QWidget, QImage, QApplication, QPainter
 from naoqi import ALProxy
 
 # To get the constants relative to the video.
@@ -11,15 +9,6 @@ import imageresolve.puzzlesolver.solver as slv
 import imageextractor.imageextractor as ext
 import imageresolve.puzzlesolver.model.puzzle as pzl
 import imageresolve.puzzlesolver.model.piece as pcl
-
-def convertimage(incomingimage):
-    incomingimage = incomingimage.convertToFormat(4)
-    width = incomingimage.width()
-    height = incomingimage.height()
-    ptr = incomingimage.bits()
-    ptr.setsize(incomingimage.byteCount())
-    arr = np.array(ptr).reshape(height, width, 4)
-    return arr
 
 if __name__ == '__main__':
     IP = "127.0.0.1"
@@ -42,10 +31,9 @@ if __name__ == '__main__':
     videoProxy.setParam(vision_definitions.kCameraSelectID, CameraID)
 
     alImage = videoProxy.getImageRemote(imgClient)
-    image = QImage(alImage[6], alImage[0], alImage[1], QImage.Format_RGB888)
-    img = convertimage(image)
 
-    img = cv2.imread("C:\\Users\\jantk_000\\Downloads\\IMG_20150422_111942_1.jpg")
+    nparr = np.fromstring(alImage[6], np.uint8).reshape(alImage[1], alImage[0], alImage[2])
+    img = cv2.cvtColor(nparr, cv2.COLOR_BGR2RGB)
 
     # cv2.imshow("Test", img)
 
